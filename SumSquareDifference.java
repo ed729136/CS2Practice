24b0c9dcf013a3aeb9e6d768e6fe53e58196ccdd
ed729136 <perezedwin991@gmail.com>
2014-11-13 17:38:24 -0500

    Optimized version of sumsquaredifference

--- SumSquareDifference.java
+++ SumSquareDifference.java
@@ -5,23 +5,20 @@ public class SumSquareDifference
 	 * therefore the implication of square of sums being greater than sum of squares exists.
 	 */
 	
-	/*
-	 * Formula to find the square of sums
-	 */
+	//Formula to find the square of sums
+	//Optimization of division by 2 using shifting bitwise operator
 	public static int SquareOfSum( int number )
 	{
-		int total = ( number * ( number + 1 ) ) / 2;
+		int total = ( number * ( number + 1 ) ) >> 1;
 		return total *= total;
 	}
 	
-	/*
-	 * Formula to find the sum of squares
-	 */
+	//Formula to find the sum of squares
+	
 	public static int SumOfSquares( int number )
 	{
-		int total = ( number * ( number + 1 ) * ( 2 * number + 1) ) / 6;
+		int total = ( number * ( number + 1 ) * ( ( number << 1 )  + 1) ) / 6;
 		return total;
-		
 	}
 	
 	public static void main( String[] args )
